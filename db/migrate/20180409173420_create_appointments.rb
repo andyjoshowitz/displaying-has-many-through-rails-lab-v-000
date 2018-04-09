c0be938362f5697b486f0fe721d1eeef93f7b025
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-04-09 17:35:24 +0000

    automatically backed up by learn

--- db/migrate/20180409173420_create_appointments.rb
+++ db/migrate/20180409173420_create_appointments.rb
@@ -2,7 +2,7 @@ class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
       t.belongs_to :patient, index: true, foreign_key: true
-      t.string :doctor
+      t.belongs_to :doctor, index: true, foreign_key: true
 
       t.timestamps null: false
     end
